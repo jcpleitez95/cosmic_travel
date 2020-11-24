70f56a0e175960e027c3d0d4be871e635f80abd8
jcpleitez95 <jcpleitez@outlook.com>
2020-11-24 15:25:22 -0500

    Done

---
+++ db/migrate/20201124192850_create_missions.rb
@@ -0,0 +1,9 @@
+class CreateMissions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :missions do |t|
+      t.string :name
+      t.integer :scientist_id
+      t.integer :planet_id
+    end
+  end
+end
